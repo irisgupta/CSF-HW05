d6172aac29b3bd8dd03768e26d0667a3a03c1bb5
Iris Gupta <iris.gupta0@gmail.com>
2022-11-29 13:47:51 -0500

    done

--- sender.cpp
+++ sender.cpp
@@ -22,7 +22,6 @@ int main(int argc, char **argv) {
   username = argv[3];
 
   // TODO: connect to server
-
   Connection connection;
   connection.connect(server_hostname, server_port);
   if (!connection.is_open()) {
@@ -31,56 +30,53 @@ int main(int argc, char **argv) {
   }
 
   // TODO: send slogin message
-
   connection.send(Message(TAG_SLOGIN, username));
   Message message;
   connection.receive(message);
   if (message.tag == TAG_ERR) {
-      std::cerr << message.data << "\n";
-      return 1;
-    }
+    std::cerr << message.data << "\n";
+    return 1;
+  }
   
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
 
   while (1) { 
-   std::string str;
-   getline(std::cin, str);
+   std::string text;
+   getline(std::cin, text);
    std::string payload;
    std::string command;
    std::stringstream ss;
-   ss << str;
+   ss << text;
    ss >> command; 
-  if (command.compare("/join") == 0) {
+  if (command == "/join") {
     ss >> payload;
-    conn.send(Message(TAG_JOIN, payload));
-    conn.receive(ok_msg);
-    if (ok_msg.tag == TAG_ERR) {
-      std::cerr << ok_msg.data  << "\n";
+    connection.send(Message(TAG_JOIN, payload));
+    connnection.receive(message);
+    if (message.tag == TAG_ERR) {
+      std::cerr << message.data  << "\n";
     }
-  } else if (command.compare("/leave") == 0) {
-      conn.send(Message(TAG_LEAVE, ""));
-      conn.receive(ok_msg);
-      if (ok_msg.tag == TAG_ERR) {
+  } else if (command == "/leave") {
+      connection.send(Message(TAG_LEAVE, ""));
+      connection.receive(message);
+      if (message.tag == TAG_ERR) {
         std::cerr << payload << "\n";
       }
-  } else if (command.compare("/quit") == 0) {
-      conn.send(Message(TAG_QUIT, ""));
-      conn.receive(ok_msg);
-    if (ok_msg.tag == TAG_ERR) {
-        std::cerr << "bye" << "\n";
+  } else if (command == "/quit") {
+      connection.send(Message(TAG_QUIT, ""));
+      connection.receive(message);
+      if (message.tag == TAG_ERR) {
+        std::cerr << "Thank you!" << "\n";
       }
       return 0;
   } else { 
-      conn.send(Message(TAG_SENDALL, str));
-      conn.receive(ok_msg);
-      if (ok_msg.tag == TAG_ERR) {
-	std::cerr << payload << "\n";
+      connection.send(Message(TAG_SENDALL, text));
+      connection.receive(message);
+      if (message.tag == TAG_ERR) {
+	      std::cerr << payload << "\n";
       }
     }
   }
   conn.close();
-  return 0;
-
   return 0;
 }
