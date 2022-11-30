fea10d9b5762a86d27bbf0f0e24f099a5f7925b8
Ericwang0912 <98420182+Ericwang0912@users.noreply.github.com>
2022-11-29 19:59:25 -0500

    Early Draft of Receiver

--- receiver.cpp
+++ receiver.cpp
@@ -21,16 +21,53 @@ int main(int argc, char **argv) {
   Connection conn;
 
   // Connect to server
-  int connfd = open_clientfd((char *)server_hostname, (char *)server_port);
+  conn.connect(server_hostname, server_port);
+  if (!conn.is_open()) {
+    std::cerr << "Connection Failed\n";
+    return 1;
+  }
   
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
-  writestr(connfd, "rlogin:");
-  writestr(connfd, username);
+  conn.send(Message(TAG_RLOGIN, username));
+  Message message;
+  conn.receive(message);
+  if (message.tag == TAG_ERR || message.tag != TAG_OK) {
+    std::cerr << message.data << "\n";
+    return 1;
+  }
+  conn.send(Message(TAG_JOIN, room_name));
+  conn.receive(message);
+  if (message.tag == TAG_ERR || message.tag != TAG_OK) {
+    std::cerr << message.data << "\n";
+    return 1;
+  }
   
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
+  while	(1) {
+    conn.receive(message);
 
+    if (message.tag == TAG_DELIVERY) {
+      std::string data = message.data;
+      std::size_t colonIndex = data.find(":");
+      std::string room = data.substr(0, colonIndex);
+      std::string senderAndMessage = data.substr(colonIndex + 1);
+      colonIndex = senderAndMessage.find(":");
+      std::string sender = senderAndMessage.substr(0, colonIndex);
+      std::string senderMessage = senderAndMessage.substr(colonIndex + 1);
+      if (room.compare(room_name) == 0) {
+        std::cout << sender << ": " << senderMessage << std::endl;
+      } else {
+        break;
+      }
+    } else {
+      std::cerr << message.data << "\n";
+      return 1;
+    }
 
+  }
+
+  conn.close();
   return 0;
 }
