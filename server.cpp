c9eed29270218cef62dbe4d909d354f9d2e818a1
Iris Gupta <iris.gupta0@gmail.com>
2022-12-09 18:14:03 -0500

    done

--- server.cpp
+++ server.cpp
@@ -43,25 +43,24 @@ void *worker(void *arg) {
   //       whatever pointer type describes the object(s) needed
   //       to communicate with a client (sender or receiver)
 
-  ConnectionData *information = static_cast<ConnectionData *>(arg);
-  std::unique_ptr<ConnectionData> data(information);
+  ConnectionData *data = static_cast<ConnectionData *>(arg);
+  std::unique_ptr<ConnectionData> dataPointer(data);
 
   // TODO: read login message (should be tagged either with
   //       TAG_SLOGIN or TAG_RLOGIN), send response
 
   Message message;
-  if (!data->connection->receive(message)) {
-    if (data->connection->get_last_result() == Connection::INVALID_MSG) {
-      data->connection->send(Message(TAG_ERR, "Invalid message!"));
+  if (!dataPointer->connection->receive(message)) {
+    if (dataPointer->connection->get_last_result() == Connection::INVALID_MSG) {
+      dataPointer->connection->send(Message(TAG_ERR, "Invalid message!"));
     }
     return nullptr;
-    
   }
   if (message.tag != TAG_RLOGIN && message.tag != TAG_SLOGIN) {
-    data->connection->send(Message(TAG_ERR, "slogin or rlogin required!"));
+    dataPointer->connection->send(Message(TAG_ERR, "slogin or rlogin required!"));
     return nullptr;
   }
-  if (!data->connection->send(Message(TAG_OK, "Hello, " + message.data))) {
+  if (!dataPointer->connection->send(Message(TAG_OK, "Hello, " + message.data))) {
     return nullptr;
   }
 
@@ -70,33 +69,29 @@ void *worker(void *arg) {
   //       separate helper functions for each of these possibilities
   //       is a good idea)
 
+  std::string room = "";
   std::string client = "";
   if (message.tag == TAG_SLOGIN) {
     client = "sender";
   }
-  else if (message.tag == TAG_RLOGIN) {
-    client = "receiver";
-  }
-  std::string room = "";
   
   while (true) {
-    if (!data->connection->receive(message)) {
-      if (data->connection->get_last_result() == Connection::INVALID_MSG) {
-        data->connection->send(Message(TAG_ERR, "Invalid message!"));
+    if (!dataPointer->connection->receive(message)) {
+      if (dataPointer->connection->get_last_result() == Connection::INVALID_MSG) {
+        dataPointer->connection->send(Message(TAG_ERR, "Invalid message!"));
       }
       break;
     } else {
       if (client == "sender") {
-        room = data->server->sender_interaction(data->connection, data->server, message, message.data);
-        if (room == "quit") {
+        roomName = dataPointer->server->sender_interaction(dataPointer->connection, dataPointer->server, message, message.data);
+        if (roomName == "quit") {
           return nullptr;
         }
       } else {
-        data->server->receiver_interaction(data->connection, data->server, message, message.data);
+        dataPointer->server->receiver_interaction(dataPointer->connection, dataPointer->server, message, message.data);
       }
     }
   }
-
   return nullptr;
 }
 
@@ -105,9 +100,9 @@ void *worker(void *arg) {
 ////////////////////////////////////////////////////////////////////////
 // Server member function implementation
 ////////////////////////////////////////////////////////////////////////
-void Server::receiver_interaction(Connection *conn, Server *server, Message msg, std::string username) {
+void Server::receiver_interaction(Message message, std::string username, Server *server, Connection *connection) {
   User member(message.data);
-  std::string room = "";
+  std::string roomName = "";
   Room *room;
   if (message.tag == TAG_JOIN) {
     roomName = message.data; 
@@ -131,13 +126,10 @@ std::string Server::sender_interaction(Connection *conn, Server *server, Message
       room = server->find_or_create_room(message.data);
       roomName = room->get_room_name();
       connection->send(Message(TAG_OK, "Okay!"));
-    } else if (message.tag == TAG_SENDALL) {
-      if(roomName == "") { 
-        connection->send(Message(TAG_ERR, "Invalid room!"));
-      } else {
-        room->broadcast_message(username, message.data);
-        connection->send(Message(TAG_OK, "Okay!"));
-      }
+    } else if (message.tag == TAG_QUIT) {
+      connection->send(Message(TAG_OK, "Time to quit!"));
+      roomName = "quit";
+      break;
     } else if (message.tag == TAG_LEAVE) {
       if(roomName == "") {
         connection->send(Message(TAG_ERR, "Invalid room!"));
@@ -145,14 +137,17 @@ std::string Server::sender_interaction(Connection *conn, Server *server, Message
         connection->send(Message(TAG_OK, "Time to leave!"));
         roomName = "";
       }
-    } else if (message.tag == TAG_QUIT) {
-      connection->send(Message(TAG_OK, "Time to quit!"));
-      roomName = "quit";
-      break;
+    } else if (message.tag == TAG_SENDALL) {
+      if(roomName == "") { 
+        connection->send(Message(TAG_ERR, "Invalid room!"));
+      } else {
+        room->broadcast_message(username, message.data);
+        connection->send(Message(TAG_OK, "Okay!"));
+      }
     } else {
-      conn->send(Message(TAG_ERR, "Invalid tag!"));
+      connection->send(Message(TAG_ERR, "Invalid tag!"));
     }
-    connection->receive(message); 
+    connection->receive(message);
   }
   return roomName;
 }
@@ -175,7 +170,12 @@ bool Server::listen() {
   //       if successful, false if not
   std::string port = std::to_string(m_port);
   m_ssock = open_listenfd(port.c_str());
-  return m_ssock;
+  if (m_ssock > 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 void Server::handle_client_requests() {
@@ -184,13 +184,13 @@ void Server::handle_client_requests() {
   while (true && m_ssock >= 0) {
     int clientfd = accept(m_ssock, nullptr, nullptr);
     if (clientfd < 0) {
-      std::cerr << "Error with accepting connection!\n";
+      std::cerr << "Connection not accepted!\n";
       return;
     }
     ConnectionData *data = new ConnectionData(new Connection(clientfd), this);
     pthread_t thr_id;
     if (pthread_create(&thr_id, nullptr, worker, static_cast<void *>(data)) != 0) {
-      std::cerr << "Could not create thread!\n";
+      std::cerr << "Error creating thread!\n";
       return;
     }
   }
@@ -199,15 +199,13 @@ void Server::handle_client_requests() {
 Room *Server::find_or_create_room(const std::string &room_name) {
   // TODO: return a pointer to the unique Room object representing
   //       the named chat room, creating a new one if necessary
-
   Guard g(m_lock);
   Room *room;
-  std::string temp = m_rooms.find(room_name);
-  if (temp == m_rooms.end()) {
+  if (m_rooms.find(room_name) == m_rooms.end()) {
     room = new Room(room_name);
     m_rooms[room_name] = room;
   } else {
-    room = temp->second;
+    room = (m_rooms.find(room_name))->second;
   }
   return room;
 }
