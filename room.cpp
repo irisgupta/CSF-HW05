81f17bfef28ef84be80672cacfd541c61064d30e
Iris Gupta <iris.gupta0@gmail.com>
2022-12-09 20:11:47 -0500

    done

--- room.cpp
+++ room.cpp
@@ -7,20 +7,34 @@
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
   // TODO: initialize the mutex
+  pthread_mutex_init(&lock, NULL);
 }
 
 Room::~Room() {
   // TODO: destroy the mutex
+  pthread_mutex_destroy(&lock);
 }
 
 void Room::add_member(User *user) {
   // TODO: add User to the room
+  Guard g(lock);
+  members.insert(user);
 }
 
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
+  Guard g(lock);
+  members.erase(user);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // TODO: send a message to every (receiver) User in the room
+  Guard g(lock);
+  std::string message = room_name + ":" + sender_username + ":" + message_text;
+  for (User *member : members) {
+    if (member->username.compare(sender_username) != 0) {
+      Message *sent = new Message(TAG_DELIVERY, message);
+      member->mqueue.enqueue(sent);
+    }
+  }
 }
